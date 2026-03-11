ba8e410275bb3f5dfa194e619dfde2f5ac6dcea5
meetmarioai <thelordofrevenge@hotmail.com>
2026-03-11 06:44:15 +0100

    force remove dashboard from git

--- app/dashboard/page.jsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-const MeetMario = dynamic(() => import('./MeetMario'), { ssr: false })
-export default function Dashboard() {
-  return <MeetMario />
-}
